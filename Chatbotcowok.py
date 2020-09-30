865c5f8cb5c7c0025a4699304ec555d936f48a86
hntrjndx <45139540+hntrjndx@users.noreply.github.com>
2020-09-30 16:12:05 +0000

    Create Chatbotcowok.py

---
+++ Chatbotcowok.py
@@ -0,0 +1,64 @@
+import time
+import sys
+print("===================================")
+print("+_______WELCOME TO CHAT BOT_______+")
+print("+_______JOMBLO WAJIB CHATAN_______+")
+print("===================================")
+#text gombal
+gombal = "Hal yang paling aku suka yaitu ngemil, namun tau gak ngemil apa yang paling aku suka? ngemilikin kamu sepenuhnya"
+
+#chat
+time.sleep(2)
+print("cowok🤴: Hi!😉")
+time.sleep(2)
+input("cewek👸: ")
+time.sleep(2)
+print("cowok🤴: Perkenalkan nama ku ada robotchat by: Mr.G2M8N05")
+time.sleep(2)
+print("cowok🤴: Nama kamu siapa?")
+nama = input("cewek👸: ")
+time.sleep(2)
+print("cowok🤴: Oh hay",nama)
+time.sleep(2)
+print("cowok🤴: Boleh kah aku bertanya?")
+time.sleep(2)
+input("cowok👸: ")
+time.sleep(2)
+print("cowok🤴: Hal apa yang kamu sukai?")
+time.sleep(2)
+input("cewek👸: ")
+time.sleep(2)
+print("cowok🤴: kalok aku,",gombal)
+time.sleep(2)
+input("cewek👸: ")
+time.sleep(2)
+print("cowok🤴: Mau lagi gak?")
+time.sleep(2)
+input("cewek👸: ")
+time.sleep(2)
+print("cowok🤴: Lagi chatan sama camer🤭")
+time.sleep(2)
+print("cowok🤴: Yaudah nanti lagi ya tunggu part 2 nya")
+
+#menu pulang
+while True:
+  time.sleep(3)
+  print("============================")
+  print("+_____SEE YOU NEXT TIME_____")
+  print("============================")
+  print("1.Keluar")
+  print("2.Lanjut")
+  menu=input("=> ")
+  
+  if menu == '1':
+    time.sleep(2)
+    print("BYE SEE YOU NEXT TIME")
+    time.sleep(2)
+    print("ChatBot by:MR.G2M8N05")
+    keluar()
+  elif menu == '2':
+    time.sleep(2)
+    print("UDAH WOI NYAMAN BANGET KEK NYA")
+    time.sleep(2)
+    print("ChatBot by:MR.G2M8N05")
+    keluar()
